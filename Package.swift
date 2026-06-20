bce177a67cbd5ce6435e0d3c52b2a4a91763ff3b
Kim David Hauser <kim@kimhauser.ch>
2026-06-20 19:56:51 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FFMpegGUIXPCServiceLib",
+    //defaultLocalization: "en", // 🌟 CRITICAL: Enables localization resource processing
     platforms: [.macOS(.v26), .iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
